3c1fdb8bc6e95160111c7f0c30806351fd0f3b76
Etienne Walch <tinouclt@gmail.com>
2019-01-23 17:47:18 +0100

    Formulaire de connexion + inscription

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,18 +2,66 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Form\CreateAccountType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="account")
+     * @Route("/login", name="account_login")
      */
-    public function index()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'pseudo' => $username
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="account_logout")
+     *
+     */
+    public function logout()
+    {
+
+    }
+
+    /**
+     * @Route("/register", name="account_register")
+     */
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new Users();
+
+        $form = $this->createForm(CreateAccountType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Vous êtes bien inscrit sur Xbox !"
+            );
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -5,11 +5,13 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
  */
-class Users
+class Users implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -32,6 +34,11 @@ class Users
      * @ORM\Column(type="string", length=255)
      */
     private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Le mot de passe n'est pas identique !")
+     */
+    public $confirmPassword;
 
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\Roles", mappedBy="users")
@@ -72,11 +79,6 @@ class Users
         return $this;
     }
 
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
     public function setPassword(string $password): self
     {
         $this->password = $password;
@@ -87,9 +89,61 @@ class Users
     /**
      * @return Collection|Roles[]
      */
-    public function getRoles(): Collection
+    public function getRoles()
     {
-        return $this->roles;
+        $roles = $this->roles->map(function($roles) {
+            return $roles->getRole();
+        })->toArray();
+
+        $roles[] = 'ROLE_USER';
+
+        return $roles;
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+
     }
 
     public function addRole(Roles $role): self
